adbbbe59985268b823f673e011110907a90a25cf
JIN <akasagi@naver.com>
2024-07-22 02:45:52 +0900

    메서드 리팩토링

--- src/main/java/com/example/storereservation/domain/store/service/StoreService.java
+++ src/main/java/com/example/storereservation/domain/store/service/StoreService.java
@@ -35,48 +35,48 @@ public class StoreService {
      * 상점 명으로 상점 정보 찾기
      * - STORE_NOT_FOUND : 해당 상점명을 가진 상점이 없을 때
      */
-    public StoreDetail findByStoreName(String name){
+    public StoreDetail findByStoreName(String name) {
         StoreEntity findStore = storeRepository.findByStoreName(name)
                 .orElseThrow(() -> new MyException(ErrorCode.STORE_NOT_FOUND));
         return StoreDetail.fromEntity(findStore);
     }
 
     /**
-     *  상점 명으로 상점 리스트 찾기
-     *  SortType : ALL / ALPHABET / RATING / REVIEW_COUNTS / DISTANCE
-     *  page : constant로 저장
+     * 상점 명으로 상점 리스트 찾기
+     * SortType : ALL / ALPHABET / RATING / REVIEW_COUNTS / DISTANCE
+     * page : constant로 저장
      */
-    public Page<StoreDetail> getStoreListByStoreNameAndSortType(StoreListQuery input, Integer page){
-
+    public Page<StoreDetail> getStoreListByStoreNameAndSortType(StoreListQuery input, Integer page) {
         PageRequest pageRequest = getPageRequestBySortTypeAndPage(input.getSortType(), page);
         Page<StoreEntity> findStores = storeRepository.findByStoreNameContaining(input.getStoreName(), pageRequest);
 
-        if(findStores.getNumberOfElements() == 0){
+        if (findStores.getNumberOfElements() == 0) {
             throw new MyException(ErrorCode.STORE_NO_SEARCH_RESULT);
         }
 
-        return findStores.map(store -> StoreDetail.fromEntity(store));
+        return findStores.map(StoreDetail::fromEntity);
     }
+
     /**
-     *  상점 명으로 상점 리스트 찾기
-     *  SortType : DISTANCE
+     * 상점 명으로 상점 리스트 찾기
+     * SortType : DISTANCE
      */
-    public List<StoreDetail> getStoreListByStoreNameAndDistance(StoreListQuery input, Integer page){
+    public List<StoreDetail> getStoreListByStoreNameAndDistance(StoreListQuery input, Integer page) {
         List<StoreDto> findStores = myBatisStoreRepository.findByStoreNameOrderByDistance(input, page);
-        if(findStores.isEmpty()){
+        if (findStores.isEmpty()) {
             throw new MyException(ErrorCode.STORE_NO_SEARCH_RESULT);
         }
 
-        return findStores.stream().map(storeDto -> StoreDetail.fromDto(storeDto)).collect(Collectors.toList());
+        return findStores.stream().map(StoreDetail::fromDto).collect(Collectors.toList());
     }
 
-    private PageRequest getPageRequestBySortTypeAndPage(StoreSortType storeSortType, Integer page){
+    private PageRequest getPageRequestBySortTypeAndPage(StoreSortType storeSortType, Integer page) {
         PageRequest pageRequest = PageRequest.of(page, PageConst.STORE_LIST_PAGE_SIZE);
-        if(storeSortType == StoreSortType.ALPHABET){
+        if (storeSortType == StoreSortType.ALPHABET) {
             return PageRequest.of(page, PageConst.STORE_LIST_PAGE_SIZE, Sort.by("storeName"));
-        }else if(storeSortType == StoreSortType.RATING){
+        } else if (storeSortType == StoreSortType.RATING) {
             return PageRequest.of(page, PageConst.STORE_LIST_PAGE_SIZE, Sort.by("rating").descending());
-        }else if(storeSortType == StoreSortType.RATING_COUNT){
+        } else if (storeSortType == StoreSortType.RATING_COUNT) {
             return PageRequest.of(page, PageConst.STORE_LIST_PAGE_SIZE, Sort.by("ratingCount").descending());
         }
         return pageRequest;
@@ -84,12 +84,12 @@ public class StoreService {
 
     /**
      * 리뷰 추가됬을 때, 매장의 리뷰 업데이트
-     * @param review : 추가된 리뷰
+     * @param review 추가된 리뷰
      */
-    public void updateRatingForAddReview(ReviewDto review){
+    public void updateRatingForAddReview(ReviewDto review) {
         StoreEntity store = storeRepository.findByStoreName(review.getStoreName())
                 .orElseThrow(() -> new MyException(ErrorCode.STORE_NOT_FOUND));
-        log.info("Update Store Rating, before update => rating : {}, count : {}", store.getRating(), store.getRatingCount());
+        log.info("Update Store Rating, before update => rating: {}, count: {}", store.getRating(), store.getRatingCount());
         Long ratingCount = store.getRatingCount();
         double rating = getNewRatingForAddReview(store.getRating(), ratingCount, review.getRating());
 
@@ -97,35 +97,32 @@ public class StoreService {
         store.setRatingCount(ratingCount + 1);
 
         StoreEntity saved = storeRepository.save(store);
-        log.info("update complete => rating : {}, count : {}", saved.getRating(),saved.getRatingCount());
+        log.info("Update complete => rating: {}, count: {}", saved.getRating(), saved.getRatingCount());
     }
 
     /**
      * 리뷰 추가 시 업데이트된 별점 반환
-     * @param rating : 매장의 기존 별점
-     * @param ratingCount : 매장의 기존 리뷰 수
-     * @param reviewRating : 새로운 리뷰의 별점
-     * @return : 업데이트 될 매장의 별점
+     * @param rating 매장의 기존 별점
+     * @param ratingCount 매장의 기존 리뷰 수
+     * @param reviewRating 새로운 리뷰의 별점
+     * @return 업데이트 될 매장의 별점
      */
-    private double getNewRatingForAddReview(double rating, Long ratingCount, double reviewRating){
-        double updatedRating = rating * ((double)ratingCount / (ratingCount + 1)) + reviewRating / (ratingCount + 1);
-        log.info("updated : {}", updatedRating);
+    private double getNewRatingForAddReview(double rating, Long ratingCount, double reviewRating) {
+        double updatedRating = rating * ((double) ratingCount / (ratingCount + 1)) + reviewRating / (ratingCount + 1);
+        log.info("Updated rating: {}", updatedRating);
         return updatedRating;
     }
 
     /**
      * 리뷰 수정 시 매장 별점 정보 업데이트
+     * @param review 수정된 리뷰
+     * @param oldRating 기존 리뷰의 별점
      */
-    public void updateRatingForEditReview(ReviewDto review, double oldRating){
+    public void updateRatingForEditReview(ReviewDto review, double oldRating) {
         StoreEntity store = storeRepository.findByStoreName(review.getStoreName())
                 .orElseThrow(() -> new MyException(ErrorCode.STORE_NOT_FOUND));
 
-        double newRating =
-                getNewRatingForEditReview(
-                        store.getRating(),
-                        store.getRatingCount(),
-                        oldRating,
-                        review.getRating());
+        double newRating = getNewRatingForEditReview(store.getRating(), store.getRatingCount(), oldRating, review.getRating());
 
         store.setRating(newRating);
 
@@ -134,13 +131,13 @@ public class StoreService {
 
     /**
      * 리뷰 수정 시 업데이트 된 별점 반환
-     * @param rating : 기존의 별점
-     * @param ratingCount : 기존의 리뷰 수
-     * @param reviewOldRating : 기존 리뷰의 별점
-     * @param reviewNewRating : 수정된 리뷰의 별점
+     * @param rating 기존의 별점
+     * @param ratingCount 기존의 리뷰 수
+     * @param reviewOldRating 기존 리뷰의 별점
+     * @param reviewNewRating 수정된 리뷰의 별점
      * @return 업데이트 될 매장의 별점
      */
-    private double getNewRatingForEditReview(double rating, Long ratingCount, double reviewOldRating, double reviewNewRating){
+    private double getNewRatingForEditReview(double rating, Long ratingCount, double reviewOldRating, double reviewNewRating) {
         return rating - (reviewOldRating - reviewNewRating) / ratingCount;
     }
 }
