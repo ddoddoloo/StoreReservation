825b2df5fb54f9952332e48eeff09d3a16c76614
JIN <akasagi@naver.com>
2024-07-22 01:48:01 +0900

    매서드 변경

--- src/main/java/com/example/storereservation/domain/partner/dto/PartnerDto.java
+++ src/main/java/com/example/storereservation/domain/partner/dto/PartnerDto.java
@@ -7,26 +7,22 @@ import java.time.LocalDateTime;
 
 @Getter
 @Setter
-@AllArgsConstructor
 @NoArgsConstructor
+@AllArgsConstructor
 @Builder
 @ToString
 public class PartnerDto {
     private String partnerId;
     private String password;
-
     private String partnerName;
     private String phone;
-
     private Long storeId;
     private String storeName;
-
-    private String memberType; //ROLE_MANAGER
-
+    private String memberType; // ROLE_MANAGER
     private LocalDateTime createAt;
     private LocalDateTime updateDt;
 
-    public static PartnerDto fromEntity(PartnerEntity partnerEntity){
+    public static PartnerDto fromEntity(PartnerEntity partnerEntity) {
         return PartnerDto.builder()
                 .partnerId(partnerEntity.getPartnerId())
                 .password(partnerEntity.getPassword())
