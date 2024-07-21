5122fed0c7b38399801cdbf750253de98e9fca28
JIN <akasagi@naver.com>
2024-07-22 03:00:26 +0900

    로그 메시지 정리

--- src/main/java/com/example/storereservation/global/interceptor/LogIn.java
+++ src/main/java/com/example/storereservation/global/interceptor/LogIn.java
@@ -10,30 +10,33 @@ import java.util.UUID;
 
 @Slf4j
 public class LogIn implements HandlerInterceptor {
-    public static String LOG_ID = "logId";
+    public static final String LOG_ID = "logId";
 
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
         String requestURI = request.getRequestURI();
         String uuid = UUID.randomUUID().toString(); // uuid 생성
-        request.setAttribute(LOG_ID, uuid); //afterCompletion에서 사용
+        request.setAttribute(LOG_ID, uuid); // afterCompletion에서 사용
 
         if (handler instanceof HandlerMethod) {
             HandlerMethod hm = (HandlerMethod) handler;
-
+            log.info("[HandlerMethod] {}", hm.getMethod().getName());
         }
-        log.info("[REQUEST URI : {}] [Handler : {}]", requestURI, handler);
 
-        return HandlerInterceptor.super.preHandle(request, response, handler);
+        log.info("[REQUEST] [URI: {}] [Handler: {}] [UUID: {}]", requestURI, handler, uuid);
+
+        return true; // 요청을 처리하기 위해 계속 진행
     }
 
     @Override
     public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
         String requestURI = request.getRequestURI();
-        String uuid = (String) request.getAttribute(LOG_ID); //preHandle에서 생성한 uuid
-        //오류발생시 오류 로그도 찍어주자.
+        String uuid = (String) request.getAttribute(LOG_ID); // preHandle에서 생성한 uuid
+
         if (ex != null) {
-            log.error("afterCompletion error!!", ex);
+            log.error("[ERROR] [URI: {}] [UUID: {}] [Exception: {}]", requestURI, uuid, ex.getMessage());
         }
+
+        log.info("[RESPONSE] [URI: {}] [UUID: {}]", requestURI, uuid);
     }
 }
