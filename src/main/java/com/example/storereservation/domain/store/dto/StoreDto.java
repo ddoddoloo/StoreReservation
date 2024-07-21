197f514ee9c875ee582b3997c64aee431a204e71
JIN <akasagi@naver.com>
2024-07-22 02:43:20 +0900

    포맷팅 개선

--- src/main/java/com/example/storereservation/domain/store/dto/StoreDto.java
+++ src/main/java/com/example/storereservation/domain/store/dto/StoreDto.java
@@ -16,22 +16,18 @@ public class StoreDto {
 
     private Long id;
     private String partnerId;
-
     private String storeName;
     private String storeAddr;
     private String text;
-
     private double lat;
     private double lnt;
     private double distance;
-
     private double rating;
     private Long ratingCount;
-
     private LocalDateTime createAt;
     private LocalDateTime updateAt;
 
-    public static StoreDto fromEntity(StoreEntity store){
+    public static StoreDto fromEntity(StoreEntity store) {
         return StoreDto.builder()
                 .id(store.getId())
                 .partnerId(store.getPartnerId())
