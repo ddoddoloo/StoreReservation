4b90395413775c022cb81a748cc061ec2db564e2
JIN <akasagi@naver.com>
2024-07-22 02:38:36 +0900

    예외 처리 개선

--- src/main/java/com/example/storereservation/domain/review/service/ReviewService.java
+++ src/main/java/com/example/storereservation/domain/review/service/ReviewService.java
@@ -29,17 +29,17 @@ public class ReviewService {
     private final ReviewRepository reviewRepository;
     private final ReservationRepository reservationRepository;
     private final UserRepository userRepository;
-
     private final StoreService storeService;
 
     /**
      * 리뷰 쓰기
-     * @param reservationId : 예약 id
-     * @param userId : 유저 id
-     * 해당 예약의 userId와 로그인 유저의 userId 일치 확인
+     * @param reservationId 예약 ID
+     * @param userId 유저 ID
+     * @param request 리뷰 작성 요청 정보
+     * @return 작성된 리뷰 DTO
      */
     @Transactional
-    public ReviewDto addReview(Long reservationId, String userId, AddReview.Request request){
+    public ReviewDto addReview(Long reservationId, String userId, AddReview.Request request) {
         ReservationEntity reservation = reservationRepository.findById(reservationId)
                 .orElseThrow(() -> new MyException(ErrorCode.RESERVATION_NOT_FOUND));
 
@@ -49,91 +49,100 @@ public class ReviewService {
         ReviewEntity review = AddReview.Request.toEntity(request, reservation);
         ReviewEntity savedReview = reviewRepository.save(review);
 
-        storeService.updateRatingForAddReview(ReviewDto.fromEntity(savedReview));//매장 리뷰 업데이트
+        storeService.updateRatingForAddReview(ReviewDto.fromEntity(savedReview)); // 매장 리뷰 업데이트
 
-        return ReviewDto.fromEntity(review);
+        return ReviewDto.fromEntity(savedReview);
     }
 
     /**
      * 해당 리뷰를 쓸 권한이 있는지 검증
+     * @param reservation 예약 엔티티
+     * @param userId 유저 ID
      */
-    private void validateReviewAvailable(ReservationEntity reservation, String userId){
-        //유저 로그인 상태가 아닌 경우
-        if(!userRepository.existsByUserId(userId)){
+    private void validateReviewAvailable(ReservationEntity reservation, String userId) {
+        if (!userRepository.existsByUserId(userId)) {
             throw new MyException(ErrorCode.USER_NOT_FOUND);
         }
-        // 해당 예약의 userID와 리뷰를 쓴 유저 ID가 일치하지 않는 경우
-        if(!reservation.getUserId().equals(userId)){
+        if (!reservation.getUserId().equals(userId)) {
             throw new MyException(ErrorCode.NO_AUTHORITY_ERROR);
         }
-        // 해당 예약에 대한 리뷰가 이미 존재하는 경우
-        if(reviewRepository.existsByReservationId(reservation.getId())){
+        if (reviewRepository.existsByReservationId(reservation.getId())) {
             throw new MyException(ErrorCode.REVIEW_ALREADY_EXIST);
         }
-        // 해당 예약이 USE_COMPLETE 상태가 아니라서 리뷰를 쓸 수 없는 경우
-        if(!reservation.getStatus().equals(ReservationStatus.USE_COMPLETE)){
+        if (!reservation.getStatus().equals(ReservationStatus.USE_COMPLETE)) {
             throw new MyException(ErrorCode.REVIEW_NOT_AVAILABLE);
         }
     }
 
     /**
      * 리뷰의 별점 범위, 텍스트 길이 검증
+     * @param rating 별점
+     * @param text 리뷰 텍스트
      */
-    private void validateReviewDetail(double rating, String text){
-        if(rating > 5 || rating < 0){
+    private void validateReviewDetail(double rating, String text) {
+        if (rating > 5 || rating < 0) {
             throw new MyException(ErrorCode.REVIEW_RATING_RANGE_ERROR);
         }
-        if(text.length() > 200){
+        if (text.length() > 200) {
             throw new MyException(ErrorCode.REVIEW_TEXT_TOO_LONG);
         }
     }
 
     /**
      * 리뷰 리스트 조회 by userId
-     * sort : 최신 순
+     * @param userId 유저 ID
+     * @param page 페이지 번호
+     * @return 유저가 작성한 리뷰 리스트 페이지
      */
-    public Page<ReviewDto> reviewListByUserId(String userId, Integer page){
+    public Page<ReviewDto> reviewListByUserId(String userId, Integer page) {
         PageRequest pageRequest = PageRequest.of(page, PageConst.REVIEW_LIST_PAGE_SIZE);
         Page<ReviewEntity> findList = reviewRepository.findByUserIdOrderByCreatedAtDesc(userId, pageRequest);
 
-        if(findList.getSize() == 0){
+        if (findList.getNumberOfElements() == 0) {
             throw new MyException(ErrorCode.REVIEW_NOT_FOUND);
         }
-        return findList.map(review -> ReviewDto.fromEntity(review));
+        return findList.map(ReviewDto::fromEntity);
     }
 
     /**
      * 리뷰 리스트 조회 by storeName
-     * sort : 최신 순, 별점 높은 순, 별점 낮은 순
+     * @param storeName 상점 이름
+     * @param sortType 정렬 타입
+     * @param page 페이지 번호
+     * @return 상점 리뷰 리스트 페이지
      */
-    public Page<ReviewDto> reviewListByStoreName(String storeName, ReviewSortType sortType, Integer page){
+    public Page<ReviewDto> reviewListByStoreName(String storeName, ReviewSortType sortType, Integer page) {
         PageRequest pageRequest = PageRequest.of(page, PageConst.REVIEW_LIST_PAGE_SIZE);
         Page<ReviewEntity> findList;
 
-        if(sortType.equals(ReviewSortType.RATING_DESC)){//별점 높은 순
+        if (sortType.equals(ReviewSortType.RATING_DESC)) {
             findList = reviewRepository.findByStoreNameOrderByRatingDesc(storeName, pageRequest);
-        } else if (sortType.equals(ReviewSortType.RATING_ASC)){ //별점 낮은 순
+        } else if (sortType.equals(ReviewSortType.RATING_ASC)) {
             findList = reviewRepository.findByStoreNameOrderByRatingAsc(storeName, pageRequest);
-        }else{//default : 최신 순
+        } else {
             findList = reviewRepository.findByStoreNameOrderByCreatedAtDesc(storeName, pageRequest);
         }
 
-        if(findList.getNumberOfElements() == 0){
+        if (findList.getNumberOfElements() == 0) {
             throw new MyException(ErrorCode.REVIEW_NOT_FOUND);
         }
-        return findList.map(review -> ReviewDto.fromEntity(review));
+        return findList.map(ReviewDto::fromEntity);
     }
 
     /**
      * 리뷰 수정
+     * @param reviewId 리뷰 ID
+     * @param userId 유저 ID
+     * @param request 리뷰 수정 요청 정보
+     * @return 수정된 리뷰 DTO
      */
-    public ReviewDto editReview(Long reviewId, String userId, EditReview.Request request){
+    public ReviewDto editReview(Long reviewId, String userId, EditReview.Request request) {
         ReviewEntity reviewEntity = reviewRepository.findById(reviewId)
                 .orElseThrow(() -> new MyException(ErrorCode.REVIEW_NOT_FOUND));
 
         double oldRating = reviewEntity.getRating();
 
-        if(!reviewEntity.getUserId().equals(userId)){
+        if (!reviewEntity.getUserId().equals(userId)) {
             throw new MyException(ErrorCode.NO_AUTHORITY_ERROR);
         }
 
@@ -142,11 +151,10 @@ public class ReviewService {
         reviewEntity.setRating(request.getRating());
         reviewEntity.setText(request.getText());
         ReviewEntity savedReview = reviewRepository.save(reviewEntity);
-        ReviewDto editedReview = ReviewDto.fromEntity(savedReview);;
+        ReviewDto editedReview = ReviewDto.fromEntity(savedReview);
 
         storeService.updateRatingForEditReview(editedReview, oldRating);
 
         return editedReview;
     }
-
 }
