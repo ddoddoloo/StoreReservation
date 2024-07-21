fe41355d95c22598e8acf67aa9e7338fed9ef059
JIN <akasagi@naver.com>
2024-07-22 02:50:04 +0900

    메서드 분리

--- src/main/java/com/example/storereservation/domain/user/service/UserService.java
+++ src/main/java/com/example/storereservation/domain/user/service/UserService.java
@@ -17,28 +17,39 @@ import javax.transaction.Transactional;
 @RequiredArgsConstructor
 @Transactional
 @Service
-public class UserService{
+public class UserService {
 
     private final UserRepository userRepository;
 
-    public UserDto register(RegisterUser.Request request){
-        if(!PasswordUtils.validatePlainTextPassword(
-                request.getPassword(), request.getPasswordCheck())){
-            throw new MyException(ErrorCode.PASSWORD_CHECK_INCORRECT);
-        }
+    /**
+     * 유저 회원가입
+     * @param request 회원가입 요청 정보
+     * @return 등록된 유저 정보
+     */
+    public UserDto register(RegisterUser.Request request) {
+        validatePassword(request.getPassword(), request.getPasswordCheck());
 
-
-        if(userRepository.existsByUserId(request.getUserId())){
+        if (userRepository.existsByUserId(request.getUserId())) {
             throw new MyException(ErrorCode.DUPLICATED_ID);
         }
 
         request.setPassword(PasswordUtils.encPassword(request.getPassword()));
 
-        UserEntity savedMember = userRepository.save(
-                RegisterUser.Request.toEntity(request));
+        UserEntity savedMember = userRepository.save(RegisterUser.Request.toEntity(request));
 
-        log.info("User register complete : {}", savedMember);
+        log.info("User register complete: {}", savedMember);
 
         return UserDto.fromEntity(savedMember);
+    }
+
+    /**
+     * 비밀번호 검증
+     * @param password 비밀번호
+     * @param passwordCheck 비밀번호 확인
+     */
+    private void validatePassword(String password, String passwordCheck) {
+        if (!PasswordUtils.validatePlainTextPassword(password, passwordCheck)) {
+            throw new MyException(ErrorCode.PASSWORD_CHECK_INCORRECT);
+        }
     }
 }
