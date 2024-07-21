ea11d90a8115f0c6384b1ee750dbc0b3fe6a8468
JIN <akasagi@naver.com>
2024-07-22 02:51:25 +0900

    주석 추가

--- src/main/java/com/example/storereservation/global/auth/controller/SecurityController.java
+++ src/main/java/com/example/storereservation/global/auth/controller/SecurityController.java
@@ -11,6 +11,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class SecurityController {
 
+    /**
+     * ACCESS_DENIED 오류 발생 시
+     * 로그인은 되어있으나 접근 권한이 없을 때
+     */
     @ApiOperation(value = "ACCESS_DENIED 오류 발생 시", notes = "로그인은 되어있으나 접근 권한이 없을 때")
     @GetMapping("/exception/auth-denied")
     public void accessDenied() {
@@ -18,10 +22,14 @@ public class SecurityController {
         throw new MyException(ErrorCode.ACCESS_DENIED);
     }
 
+    /**
+     * LOGIN_REQUIRED 오류 발생 시
+     * 로그인 되지 않은 상태일 때
+     */
     @ApiOperation(value = "LOGIN_REQUIRED 오류 발생 시", notes = "로그인 되지 않은 상태일 때")
     @GetMapping("/exception/unauthorized")
     public void unauthorized() {
-        log.info("LOGIN_REQIRED - SecurityController");
+        log.info("LOGIN_REQUIRED - SecurityController");
         throw new MyException(ErrorCode.LOGIN_REQUIRED);
     }
 }
