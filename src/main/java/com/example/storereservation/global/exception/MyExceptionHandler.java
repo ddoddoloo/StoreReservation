0b687d2007d235fdee1d2141f06996ca7ebea830
JIN <akasagi@naver.com>
2024-07-22 02:59:37 +0900

    로깅 추가, http 응답 상태 코드 설정

--- src/main/java/com/example/storereservation/global/exception/MyExceptionHandler.java
+++ src/main/java/com/example/storereservation/global/exception/MyExceptionHandler.java
@@ -1,6 +1,8 @@
 package com.example.storereservation.global.exception;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -8,11 +10,15 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 @RestControllerAdvice
 public class MyExceptionHandler {
 
+    /**
+     * MyException 예외 처리 핸들러
+     * @param e 발생한 MyException 예외
+     * @return 에러 응답 객체
+     */
     @ExceptionHandler(MyException.class)
-    protected ErrorResponse handleMyException(MyException e){
+    protected ResponseEntity<ErrorResponse> handleMyException(MyException e) {
+        log.error("MyException 발생: {}", e.getMessage());
         ErrorResponse errorResponse = new ErrorResponse(e.getErrorCode());
-        return errorResponse;
+        return new ResponseEntity<>(errorResponse, HttpStatus.valueOf(e.getErrorCode().getStatusCode()));
     }
-
-
 }
