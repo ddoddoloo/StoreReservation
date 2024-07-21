6775524653668506a36c8b4aa69c4a579c6fe3a9
JIN <akasagi@naver.com>
2024-07-22 02:43:13 +0900

    주석 추가

--- src/main/java/com/example/storereservation/domain/store/mybatis/MyBatisStoreRepository.java
+++ src/main/java/com/example/storereservation/domain/store/mybatis/MyBatisStoreRepository.java
@@ -11,15 +11,21 @@ import java.util.List;
 @Repository
 @RequiredArgsConstructor
 public class MyBatisStoreRepository {
+
     private final StoreMapper storeMapper;
 
-    public List<StoreDto> findByStoreNameOrderByDistance(StoreListQuery input, Integer page){
+    /**
+     * 매장 이름으로 거리 순 정렬된 매장 리스트 조회
+     * @param input 매장 검색 조건
+     * @param page 페이지 번호
+     * @return 거리 순으로 정렬된 매장 리스트
+     */
+    public List<StoreDto> findByStoreNameOrderByDistance(StoreListQuery input, Integer page) {
         Integer size = PageConst.STORE_LIST_PAGE_SIZE;
-        return
-                storeMapper.findStoreListOrderByDistance(
-                        input.getStoreName(),
-                        input.getLat(),
-                        input.getLnt(),
-                        size * page, size);
+        return storeMapper.findStoreListOrderByDistance(
+                input.getStoreName(),
+                input.getLat(),
+                input.getLnt(),
+                size * page, size);
     }
 }
